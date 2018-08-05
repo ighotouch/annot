ac28b12a100282e777a476da6daacf35f81573bd
Matthew Igho <ighotouch@gmail.com>
2018-08-05 17:23:25 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+Amplify.config(configuration);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
